ae828b6a6d39fae7808b463fff755cf1f226ead0
Snehabai <167179562+Snehabai@users.noreply.github.com>
2024-04-20 19:18:21 +0530

    Add files via upload

---
+++ oops.py
@@ -0,0 +1,19 @@
+class Mango:
+    def _init_(self):
+        print("this is what")
+    def balaji(self):
+        print("this is without parameters")
+    def shetty(self, a,b):
+        print(a+b,"this is with para")
+    def magicalprime(self, a):
+        print("check it magical prime or not")
+    def neon(self,a):
+        print("neon or not")
+man=Mango()
+man.balaji()
+man.shetty(10,20.5)
+man.magicalprime(101)
+man.neon(7)
+        
+    
+    
